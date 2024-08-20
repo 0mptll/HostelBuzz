c7a05933fcc2f2af483a89941021059fb8fce572
Om Patel <144597861+0mptll@users.noreply.github.com>
2024-08-20 19:44:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'food_vote_screen.dart';
 import 'review_screen.dart';
+import 'maintenance_request_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HostelBuzz',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -20,6 +24,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => HomeScreen(),
         '/foodVote': (context) => FoodVoteScreen(),
         '/reviews': (context) => ReviewScreen(),
+        '/maintenance': (context) => MaintenanceRequestScreen(),
       },
     );
   }
